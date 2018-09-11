dfa1efbb55585a8e7c75d9e1419b5dec4246f0b5
Emanuele <emanuele.vannacci@gmail.com>
2018-09-11 15:31:32 +0200

    Non-Blocking read/write

--- module/mailSlot.c
+++ module/mailSlot.c
@@ -40,6 +40,9 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Emanuele Vannacci");
+MODULE_DESCRIPTION("MailSlot service via dev file driver");
+
+#define MODNAME "Mailslot"
 
 static ssize_t mailslot_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t mailslot_write(struct file *, const char __user *, size_t, loff_t *);
@@ -47,19 +50,144 @@ static long mailslot_ioctl(struct file *, unsigned int, unsigned long);
 static int mailslot_open(struct inode *, struct file *);
 static int mailslot_release(struct inode *, struct file *);
 
+#define DEBUG if(1)
 #define DEVICE_NAME "MailSlot"
+#define MSG_SZ 256
+#define MAILSLOT_SZ (MSG_SZ * 1000)
+#define N_INSTANCES 256
 
 static int Major;
+static unsigned int max_msg_size = MSG_SZ;
+int errno;
 
-static ssize_t mailslot_read(struct file *filp, char __user *buff, size_t len, loff_t *off)
+typedef struct mailslot_msg_t {
+        struct mailslot_msg_t *next;
+        char *data;
+        unsigned int size;
+} mailslot_msg_t;
+
+
+typedef struct mailslot_t {
+        mailslot_msg_t *first;
+        mailslot_msg_t *last;
+        size_t size;
+	struct mutex *mtx;
+	wait_queue_head_t reader_queue;
+	wait_queue_head_t writer_queue;
+} mailslot_t;
+
+static mailslot_t mailslot[N_INSTANCES];
+
+static ssize_t push(int minor, mailslot_msg_t *msg)
 {
-	return 0;
+	mutex_lock(mailslot[minor].mtx);
+
+	// message cannot be accepted in the mailslot: Non-Blocking behavior
+	if(mailslot[minor].size + msg->size > MAILSLOT_SZ){
+		printk(KERN_INFO "%s: message cannot be accepted because size exceed\n", MODNAME);
+		kfree(msg);
+		return -ERANGE;
+	}
+
+	if(mailslot[minor].last == NULL){
+		mailslot[minor].first = msg;
+		mailslot[minor].last = msg;
+		mutex_unlock(mailslot[minor].mtx);
+		return 0;
+	}
+
+	mailslot[minor].last->next = msg;
+	mailslot[minor].last = msg;
+	msg->next = NULL;
+	mailslot[minor].size += msg->size;
+
+	mutex_unlock(mailslot[minor].mtx);
+
+	return msg->size;
 }
 
+static mailslot_msg_t *pull(int minor, size_t len)
+{
+	mailslot_msg_t *msg;
+
+	mutex_lock(mailslot[minor].mtx);
+
+	// no msg to read: Non-Blocking behavior
+	if(mailslot[minor].size <= 0 || mailslot[minor].size > len){
+		errno = ENOMSG;
+		return NULL;
+	}
+
+	msg = mailslot[minor].first;
+	mailslot[minor].first = mailslot[minor].first->next;
+	mailslot[minor].size -= msg->size;
+
+	mutex_unlock(mailslot[minor].mtx);
+	return msg;
+}
 
 static ssize_t mailslot_write(struct file *filp, const char *buff, size_t len, loff_t *off)
 {
-	return 0;
+	mailslot_msg_t *msg;
+	unsigned long ret;
+
+	if(len > max_msg_size){
+		DEBUG
+		printk(KERN_ERR "%s: messagge too long\n",MODNAME);
+		return -EMSGSIZE;
+	}
+
+	msg = kmalloc(sizeof(mailslot_msg_t), GFP_KERNEL);
+	if(!msg){
+		DEBUG
+		printk(KERN_ERR "%s: memory allocation error\n", MODNAME);
+		return -ENOMEM;
+	}
+
+	msg->size = len;
+	msg->data = kmalloc(len, GFP_KERNEL);
+	if(!msg->data){
+		DEBUG
+		printk(KERN_ERR "%s: memory allocation error\n", MODNAME);
+		return -ENOMEM;
+	}
+
+	ret = copy_from_user(msg->data, buff, len);
+	if(!ret){
+		DEBUG
+		printk(KERN_ERR "%s: memory coping from user error\n",MODNAME);
+		return -ENOMEM;
+	}
+
+	return push(MINOR(filp->f_path.dentry->d_inode->i_rdev), msg);
+
+}
+
+
+static ssize_t mailslot_read(struct file *filp, char __user *buff, size_t len, loff_t *off)
+{
+	int ret;
+	int minor;
+	mailslot_msg_t *msg;
+
+	minor = MINOR(filp->f_path.dentry->d_inode->i_rdev);
+
+	errno = 0;
+	msg = pull(minor, len);
+	if(msg == NULL){
+		if(errno == ENOMSG)
+			printk(KERN_ERR "%s: no message to read\n", MODNAME);
+		return -errno ;
+	}
+
+	ret = copy_to_user(buff, msg->data, msg->size);
+	if(!ret){
+		DEBUG
+		printk(KERN_ERR "%s: error delivering data to user\n", MODNAME);
+		return -ENOMEM;
+	}
+
+	return len;
 }
 
 
@@ -90,7 +218,7 @@ static struct file_operations fops = {
 
 int init_module(void)
 {
-
+	int i;
 	Major = register_chrdev(0, DEVICE_NAME, &fops);
 
 	if (Major < 0) {
@@ -100,6 +228,11 @@ int init_module(void)
 
 	printk(KERN_INFO "Mailslot device registered, it is assigned major number %d\n", Major);
 
+	for(i = 0; i < N_INSTANCES; i++){
+		mutex_init(mailslot[i].mtx);
+		init_waitqueue_head(&mailslot[i].reader_queue);
+		init_waitqueue_head(&mailslot[i].writer_queue);
+	}
 
 	return 0;
 }
